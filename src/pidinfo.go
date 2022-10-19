fde2e33642250a8fe5d447ed85349a8bd24fe415
astalios <37106010+Astalios@users.noreply.github.com>
2022-10-19 21:33:47 +0200

    avancees sur la recuperation des PID

--- src/pidinfo.go
+++ src/pidinfo.go
@@ -1,14 +1,12 @@
-// Coded by TD, SD & KD the 10/10/22
-// Memory & CPU info
 package main
 
 import (
-	"bufio"
+//	"bufio"
 	"fmt"
 	"os"
 	"io"
 	"strconv"
-	"strings"
+//	"strings"
 )
 
 type Pid struct {
@@ -19,35 +17,33 @@ type Pid struct {
 	memory int
 }
 
-func getProcess() []int {
+func getProcess() []string {
 
-	dir, err : = os.Open("/proc")
+	dir, err := os.Open("/proc")
 	if err != nil {
 		panic(err)
 	}
 	
 	defer dir.Close()
 
-	
-	onlydir := int[]
+	var onlydir []string
 	for {
-		all, err = dir.Readdirnames(0) // list all ( 0 ) files in the directory.
-		// error occurs when you arrive at the end of the directory content
+		all, err := dir.Readdirnames(0) 	// list all ( 0 ) files in the directory.
+							// error occurs when you arrive at the end of the directory content
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
 			panic(err)
 		}
-		// stores in the onlydir array files that are dir
-		for _, name := range all { // Itteration du dossier contenu dans all
-			fmt.Println(name) 
-			
-			fileInfo, err := name.Stat() // donnees sur le directory en question, si erreur alors panic
+							// stores in the onlydir array files that are dir
+		for _, name := range all { 		// Itteration du dossier contenu dans all
+			path := "/proc/" + name
+			fileInfo, err := os.Stat(path) 	// donnees sur le directory en question, si erreur alors panic
 			if err != nil{
 				panic(err)
 			}
-			if _, err := strconv.Atoi(name); err == nil { // on ne fait qu'un append si le fichier est un chiffre
+			if _ , err := strconv.Atoi(name); err != nil { // on ne fait qu'un append si le fichier contient des chiffre
 				if fileInfo.IsDir() { // et si c'est un dossier
 					onlydir = append(onlydir, name) // append du name dans only dir
 				} else {
@@ -56,18 +52,20 @@ func getProcess() []int {
 			}
 		}
 	}
+	return onlydir
 }
 
 
 func pid(){
 	
-	//fmt.Println(pid_max())
+	fmt.Println(getProcess())
 
 //	file, err := os.Open("/proc/")
 }
 
 
 func main(){
-	fmt.Println(pid())
+	pid()
+//	fmt.Println(pid())
 }
 
