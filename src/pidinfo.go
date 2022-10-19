c44e3e0b59b80268db95a9fd6954167d1c336423
astalios <37106010+Astalios@users.noreply.github.com>
2022-10-20 00:08:46 +0200

    pidinfo

--- src/pidinfo.go
+++ src/pidinfo.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-//	"bufio"
+	//	"bufio"
 	"fmt"
-	"os"
 	"io"
+	"os"
 	"strconv"
-//	"strings"
+	// "strings"
 )
 
 type Pid struct {
-	name string
+	name   string
 	number int
-	cwd string
-	exe string
+	cwd    string
+	exe    string
 	memory int
 }
+
+//TODO fonction de boucle pour recuperer les informations processeurs CF structure
+// NE PAS OUBLIER LES LIENS SYMBOLIQUES
+//TODO fonction pour formater les valeurs dans une seule string
 
 func getProcess() []string {
 
@@ -23,28 +27,29 @@ func getProcess() []string {
 	if err != nil {
 		panic(err)
 	}
-	
+
 	defer dir.Close()
 
 	var onlydir []string
 	for {
-		all, err := dir.Readdirnames(0) 	// list all ( 0 ) files in the directory.
-							// error occurs when you arrive at the end of the directory content
+		all, err := dir.Readdirnames(0) // list all ( 0 ) files in the directory.
+		// error occurs when you arrive at the end of the directory content
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
 			panic(err)
 		}
-							// stores in the onlydir array files that are dir
-		for _, name := range all { 		// Itteration du dossier contenu dans all
+		// stores in the onlydir array files that are dir
+		for _, name := range all { // Itteration du dossier contenu dans all
 			path := "/proc/" + name
-			fileInfo, err := os.Stat(path) 	// donnees sur le directory en question, si erreur alors panic
-			if err != nil{
+			fileInfo, err := os.Stat(path) // donnees sur le directory en question, si erreur alors panic
+			if err != nil {
 				panic(err)
 			}
-			if _ , err := strconv.Atoi(name); err != nil { // on ne fait qu'un append si le fichier contient des chiffre
+			if _, err := strconv.Atoi(name); err == nil { // on ne fait qu'un append si le fichier contient des chiffre
 				if fileInfo.IsDir() { // et si c'est un dossier
+					fmt.Println(name)
 					onlydir = append(onlydir, name) // append du name dans only dir
 				} else {
 					continue // sinon on next
@@ -52,20 +57,17 @@ func getProcess() []string {
 			}
 		}
 	}
-	return onlydir
+	// TODO: return le slice
 }
 
+func pid() {
 
-func pid(){
-	
 	fmt.Println(getProcess())
 
-//	file, err := os.Open("/proc/")
+	// file, err := os.Open("/proc/")
 }
 
-
-func main(){
+func main() {
 	pid()
-//	fmt.Println(pid())
+	// fmt.Println(pid())
 }
-
