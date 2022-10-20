d571e474aab40a34967d087ef229a37c5e6e4b02
astalios <37106010+Astalios@users.noreply.github.com>
2022-10-20 11:44:12 +0200

    fini pid

--- src/pidinfo.go
+++ src/pidinfo.go
@@ -1,23 +1,63 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"os"
+	"strings"
 	"strconv"
 )
 
-type Pid struct {
-	name   string
-	number int
-	cwd    string
-	exe    string
-	memory int
-}
 
 //TODO fonction de boucle pour recuperer les informations processeurs CF structure
 // NE PAS OUBLIER LES LIENS SYMBOLIQUES
 //TODO fonction pour formater les valeurs dans une seule string
+
+func parser(line string) (key string, value string){
+	text := strings.ReplaceAll(line[:len(line)], " ", "")
+	values := strings.Split(text, ":")
+	return values[0], values[1]
+}
+
+func getPidInfo(pidArray []string) []string {
+
+	var processConcat []string
+
+	for _, name := range pidArray {
+		pidDir := "/proc/"+name
+		fileName := "/proc/" + name + "/status"
+		file, err := os.Open(fileName)
+		if err != nil{
+			panic(err)
+		} 
+		scanner := bufio.NewScanner(file)
+		var processInfo []string
+		for scanner.Scan(){
+			key, value := parser(scanner.Text())
+			switch key {
+			case "Name":
+				processInfo = append(processInfo, "NAME: " + value)
+			case "Pid":
+				processInfo = append(processInfo, " PID:  " + value)
+			case "VmRSS":
+				processInfo = append(processInfo, " RAM:  " + value)
+			}
+		}
+		dir, _ := os.Open(pidDir)
+		exe, _ := os.Readlink(pidDir+"/exe")
+		cwd, err := os.Readlink(pidDir+"/cwd")
+		formatString := " exe: "+exe+" cwd:  "+cwd+"\n"
+
+		processConcat = append(processConcat, processInfo...)
+		processConcat = append(processConcat, formatString)
+		file.Close()
+		dir.Close()
+	}	
+	
+
+	return processConcat
+}
 
 func getProcess() []string {
 
@@ -29,7 +69,7 @@ func getProcess() []string {
 	defer dir.Close()
 	var onlydir []string
 	for {
-		all, err := dir.Readdirnames(10) // list all ( 0 ) files in the directory.
+		all, err := dir.Readdirnames(10) // list all files in the directory, slice per slice .
 		// error occurs when you arrive at the end of the directory content
 		if err == io.EOF {
 			break
@@ -55,11 +95,15 @@ func getProcess() []string {
 	}
 	return onlydir
 }
+
 func pid() {
 
-	fmt.Println(getProcess())
+	processList := getProcess()
 
-	// file, err := os.Open("/proc/")
+	fmt.Println(getPidInfo(processList))
+
+	//fmt.Println(getProcess())
+
 }
 
 func main() {
